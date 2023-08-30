06d6025c8a51bfac59e72c8b14bbc021dcca6550
Benjamin Telsing <Benjamin.telsing123@gmail.com>
2023-08-30 11:53:02 +0200

    Changed seeddata

--- SurfsUp/SurfsUp/Data/SeedData.cs
+++ SurfsUp/SurfsUp/Data/SeedData.cs
@@ -32,7 +32,7 @@ namespace SurfsUp.Data
                         Length = "6",
                         Width = "21",
                         Thickness = "2,75",
-                        Volume = "38.8",
+                        Volume = "38,8",
                         Type = Models.Type.Shortboard,
                         Price = 565,
                         Equipment = null
@@ -41,26 +41,26 @@ namespace SurfsUp.Data
                 newBoards.Add(context.Board.Add(
     new Board
     {
-        Name = "The Minilog",
-        Length = "6",
-        Width = "21",
+        Name = "The Wide Glider",
+        Length = "7,1",
+        Width = "21,75",
         Thickness = "2,75",
-        Volume = "38.8",
-        Type = Models.Type.Shortboard,
-        Price = 565,
+        Volume = "44,16",
+        Type = Models.Type.Funboard,
+        Price = 685,
         Equipment = null
     }).Entity);
 
                 newBoards.Add(context.Board.Add(
     new Board
     {
-        Name = "The Minilog",
-        Length = "6",
-        Width = "21",
-        Thickness = "2,75",
-        Volume = "38.8",
-        Type = Models.Type.Shortboard,
-        Price = 565,
+        Name = "The Golden Ratio",
+        Length = "6,3",
+        Width = "21,85",
+        Thickness = "2,9",
+        Volume = "43,22",
+        Type = Models.Type.Funboard,
+        Price = 695,
         Equipment = null
     }).Entity);
 
@@ -68,92 +68,92 @@ namespace SurfsUp.Data
                 newBoards.Add(context.Board.Add(
     new Board
     {
-        Name = "The Minilog",
-        Length = "6",
-        Width = "21",
-        Thickness = "2,75",
-        Volume = "38.8",
-        Type = Models.Type.Shortboard,
-        Price = 565,
+        Name = "Mahi Mahi",
+        Length = "5,4",
+        Width = "20,75",
+        Thickness = "2,3",
+        Volume = "29,39",
+        Type = Models.Type.Fish,
+        Price = 645,
         Equipment = null
     }).Entity);
 
                 newBoards.Add(context.Board.Add(
     new Board
     {
-        Name = "The Minilog",
-        Length = "6",
-        Width = "21",
-        Thickness = "2,75",
-        Volume = "38.8",
-        Type = Models.Type.Shortboard,
-        Price = 565,
+        Name = "The Emerald Glider",
+        Length = "9,2",
+        Width = "22,8",
+        Thickness = "2,8",
+        Volume = "65,4",
+        Type = Models.Type.Longboard,
+        Price = 895,
         Equipment = null
     }).Entity);
 
                 newBoards.Add(context.Board.Add(
     new Board
     {
-        Name = "The Minilog",
-        Length = "6",
+        Name = "The Bomb",
+        Length = "5,5",
         Width = "21",
-        Thickness = "2,75",
-        Volume = "38.8",
+        Thickness = "2,5",
+        Volume = "33,7",
         Type = Models.Type.Shortboard,
-        Price = 565,
+        Price = 645,
         Equipment = null
     }).Entity);
 
                 newBoards.Add(context.Board.Add(
     new Board
     {
-        Name = "The Minilog",
-        Length = "6",
-        Width = "21",
-        Thickness = "2,75",
-        Volume = "38.8",
-        Type = Models.Type.Shortboard,
-        Price = 565,
+        Name = "Walden Magic",
+        Length = "9,6",
+        Width = "19,4",
+        Thickness = "3",
+        Volume = "80",
+        Type = Models.Type.Longboard,
+        Price = 1025,
         Equipment = null
     }).Entity);
 
                 newBoards.Add(context.Board.Add(
     new Board
     {
-        Name = "The Minilog",
-        Length = "6",
-        Width = "21",
-        Thickness = "2,75",
-        Volume = "38.8",
-        Type = Models.Type.Shortboard,
-        Price = 565,
-        Equipment = null
+        Name = "Naish One",
+        Length = "12,6",
+        Width = "30",
+        Thickness = "6",
+        Volume = "301",
+        Type = Models.Type.SUP,
+        Price = 854,
+        Equipment = "Paddle"
     }).Entity);
 
                 newBoards.Add(context.Board.Add(
     new Board
     {
-        Name = "The Minilog",
-        Length = "6",
-        Width = "21",
-        Thickness = "2,75",
-        Volume = "38.8",
-        Type = Models.Type.Shortboard,
-        Price = 565,
-        Equipment = null
+        Name = "Six Tourer",
+        Length = "11,6",
+        Width = "32",
+        Thickness = "6",
+        Volume = "270",
+        Type = Models.Type.SUP,
+        Price = 611,
+        Equipment = "Fin, Paddle, Pump, Leash"
     }).Entity);
 
                 newBoards.Add(context.Board.Add(
     new Board
     {
-        Name = "The Minilog",
-        Length = "6",
-        Width = "21",
-        Thickness = "2,75",
-        Volume = "38.8",
-        Type = Models.Type.Shortboard,
-        Price = 565,
-        Equipment = null
+        Name = "Naish Maliko",
+        Length = "14",
+        Width = "25",
+        Thickness = "6",
+        Volume = "330",
+        Type = Models.Type.SUP,
+        Price = 1304,
+        Equipment = "Fin, Paddle, Pump, Leash"
     }).Entity);
 
                 context.SaveChanges();
