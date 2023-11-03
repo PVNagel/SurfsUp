0e1a7402805b5751d238118ebbdedec7c0a69422
PVNagel <philip.v.nagel@gmail.com>
2023-11-03 09:41:04 +0100

    Blazor implementation

---
+++ SurfsUp/SurfsUpShop/Program.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using SurfsUpShop.Web.BlazorWasm;
+using SurfsUpShop.Libraries.Services.Product;
+using SurfsUpShop.Libraries.Services.Storage;
+using SurfsUpShop.Libraries.Services.ShoppingCart;
+using SurfsUpShop;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<IStorageService, StorageService>();
+builder.Services.AddSingleton<IShoppingCartService, ShoppingCartService>();
+builder.Services.AddTransient<IProductService, ProductService>();
+
+await builder.Build().RunAsync();
