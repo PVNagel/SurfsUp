556eb58b6739fc9f83ec3569058c8de902b632d4
PVNagel <philip.v.nagel@gmail.com>
2023-11-28 11:35:01 +0100

    Added most classes and help page

---
+++ SurfsUp/SurfsUpClassLibrary/Help/Models/LiveChatMessage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurfsUpClassLibrary.Help.Models
+{
+    public class LiveChatMessage
+    {
+        public string Message { get; }
+
+        public LiveChatTypeEnum Type { get; }
+
+        public string ClassName
+        {
+            get
+            {
+                switch (Type)
+                {
+                    case LiveChatTypeEnum.SENT:
+                        return "sent";
+                    case LiveChatTypeEnum.RECEIVED:
+                        return "received";
+                }
+
+                return "";
+            }
+        }
+
+        public LiveChatMessage(string message, LiveChatTypeEnum type)
+        {
+            Message = message;
+            Type = type;
+        }
+    }
+}
